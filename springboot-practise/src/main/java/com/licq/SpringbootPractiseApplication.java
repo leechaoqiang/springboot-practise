91e8fb9a4530a568d8935b6bf38279eca0ad0c7e
leechaoqiang <Aristotlelee@126.com>
2016-08-08 11:40:41 +0800

    init pro...

---
+++ springboot-practise/src/main/java/com/licq/SpringbootPractiseApplication.java
@@ -0,0 +1,12 @@
+package com.licq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPractiseApplication.class, args);
+	}
+}
